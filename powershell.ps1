b776fb93f3d984dea65020b6858211de585875b4
fernandoavila44 <zuluxray44@gmail.com>
2024-05-28 19:56:39 -0500

    feat-third-push

--- powershell.ps1
+++ powershell.ps1
@@ -1,17 +1,14 @@
-# Obtención de información del sistema:
-$computerName = $env:COMPUTERNAME
+# 1. Crear una funcion que me diga si un número es par o no
 
-$os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computerName
-$processor = Get-WmiObject -Class Win32_Processor -ComputerName $computerName
+# 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
+# Devuelve Fizz si el número es divisible por 3.
+# Devuelve Buzz si el número es divisible por 5.
+# Devuelve FizzBuzz si el número es divisible por 3 y por 5.
 
-$systemInfo = @{
-  "Sistema Operativo" = $os.Caption
-  "Versión"           = $os.Version
-  "Arquitectura"      = $os.OSArchitecture
-  "Procesador"        = $processor.Name
-  "Núcleos"           = $processor.NumberOfCores
-}
+# 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
+# $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-$systemInfo | Format-Table -AutoSize
+# 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
+# 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
 
 
