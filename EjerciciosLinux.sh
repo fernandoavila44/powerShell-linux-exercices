5d8d66e61ae8ed46327775495c4a9f2f47cd8172
DrowAnn <rs19990801@gmail.com>
2024-05-30 15:08:22 -0500

    Finalizacion de actividades

--- EjerciciosLinux.sh
+++ EjerciciosLinux.sh
@@ -1,10 +1,12 @@
+#!bin/bash
+
 clear
 
 ###Taller Linux
 
 # #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
 
-curl https://jsonplaceholder.typicode.com/users | jq
+curl https://jsonplaceholder.typicode.com/users | jq .
 
 #----------------------------------------------------------------------
 
@@ -22,7 +24,7 @@ curl https://jsonplaceholder.typicode.com/posts | jq '.[] | select(.id==67)'
 
 # #4. Utiliza curl para enviar un nuevo POST a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
 
-curl -X POST -d '{"userId":123, "id":155, "title":"Prueba Robert para Actividad","body":"Esta es una prueba para validar la metodo POST a la API de pruebas"}' -H "Content-Type: application/json" https://jsonplaceholder.typicode.com/posts | jq
+curl -X POST -d '{"userId":123, "id":155, "title":"Prueba Robert para Actividad","body":"Esta es una prueba para validar la metodo POST a la API de pruebas"}' -H "Content-Type: application/json" https://jsonplaceholder.typicode.com/posts | jq .
 
 #----------------------------------------------------------------------
 
