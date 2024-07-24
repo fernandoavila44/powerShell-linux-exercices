578a57afbe02d12176f10986c9c9f93c9b97d862
Walter Ardila R <w.a.r15@hotmail.com>
2024-07-24 14:17:27 -0500

    Realizado punto 3

--- Powershell/taller.ps1
+++ Powershell/taller.ps1
@@ -37,6 +37,25 @@ for ($i = 1; $i -le 100; $i++) {
 # 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
 # $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+ 
+$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+function ExisteNumero {
+    param ([int]$numero,[int[]]$arreglo)
+
+    if ($arreglo -contains $numero) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+$numero = 11
+if (ExisteNumero -numero $numero -arreglo $numeros) {
+    Write-Output "El numero $numero existe."
+} else {
+    Write-Output "El numero $numero no existe."
+}
+
 # 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
 # 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
 
