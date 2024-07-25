23d3efa8aaa92fb01b5d132fa66d649fbde173e8
NicolasRivera <nicolasrpv02@gmail.com>
2024-07-25 12:17:15 -0500

    Tercer  ejercicio linux mostrar post  por id

--- Linux/linux.sh
+++ Linux/linux.sh
@@ -17,10 +17,17 @@ echo "$response" | jq -c '.[]' | while read -r titles; do
 done
  }
  #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un id específico.
+   MostrarTitulosPorId(){
+    POST_ID=20
+    echo "Obteniendo datos de la API..."
+response=$(curl -s "https://jsonplaceholder.typicode.com/posts" | jq ".[] | select(.id == $POST_ID)")
+echo $response | jq
+ }
  #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
  #5. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/"cualquier endpoint" que devuelva un error intencionalmente. Utiliza jq para detectar y manejar el error, mostrando un mensaje personalizado en caso de fallo.
 
  #Exitos!!
 
 #MostrarUsuarios
-MostrarTitulos
+#MostrarTitulos
+MostrarTitulosPorId
