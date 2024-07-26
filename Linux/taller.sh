2dcf6a3ccfa2efa5daf9d5dbb59b81cfd50a8952
Walter Ardila R <w.a.r15@hotmail.com>
2024-07-26 16:19:21 -0500

    Realizo taller.sh punto 5 Corrgido

--- Linux/taller.sh
+++ Linux/taller.sh
@@ -5,60 +5,53 @@
  #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para 
  #formatear y mostrar la lista de usuarios.
 
-#echo " "
-#echo "Obteniendo datos de la API..."
-#response=$(curl https://jsonplaceholder.typicode.com/users)
+echo " "
+echo "Obteniendo datos de la API..."
+response=$(curl https://jsonplaceholder.typicode.com/users)
 
-#echo " "
-#echo "Datos obtenidos:"
-#echo "$response" | jq .
+echo " "
+echo "Datos obtenidos:"
+echo "$response" | jq .
 
  #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para 
  #filtrar y mostrar solo los títulos de las publicaciones.
 
-#echo " "
-#echo "Obteniendo datos de la API..."
-#response=$(curl https://jsonplaceholder.typicode.com/posts)
+echo " "
+echo "Obteniendo datos de la API..."
+response=$(curl https://jsonplaceholder.typicode.com/posts)
 
-#echo " "
-#echo "$response" | jq -c '.[]' | while read -r post; do
-#    title=$(echo "$post" | jq -r '.title')
-#    echo "Titulo del post: $title"
-#done
+echo " "
+echo "$response" | jq -c '.[]' | while read -r post; do
+   title=$(echo "$post" | jq -r '.title')
+   echo "Titulo del post: $title"
+done
 
  #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para 
  #encontrar y mostrar la publicación con un id específico.
 
-# echo " "
-# echo "Obteniendo datos de la API..."
-# response=$(curl https://jsonplaceholder.typicode.com/posts)
+echo " "
+echo "Obteniendo datos de la API..."
+response=$(curl https://jsonplaceholder.typicode.com/posts)
 
-# echo " "
-# echo "$response" | jq '.[] | select(.id == 34)'
+echo " "
+echo "$response" | jq '.[] | select(.id == 34)'
 
  #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, 
  #utiliza jq para mostrar la respuesta del servidor.
 
-# echo " "
-# data='{"title": "foo", "body": "bar", "userId": 1}'
-# echo "Enviando datos a la API..."
-# response=$(curl -s -X POST -H "Content-Type: application/json" -d "$data" https://jsonplaceholder.typicode.com/posts)
+echo " "
+data='{"title": "foo", "body": "bar", "userId": 1}'
+echo "Enviando datos a la API..."
+response=$(curl -s -X POST -H "Content-Type: application/json" -d "$data" https://jsonplaceholder.typicode.com/posts)
 
-# echo " "
-# echo "Datos obtenidos:"
-# echo "$response" | jq .
+echo " "
+echo "Datos obtenidos:"
+echo "$response" | jq .
 
  #5. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/"cualquier endpoint" 
  #que devuelva un error intencionalmente. Utiliza jq para detectar y manejar el error, 
  #mostrando un mensaje personalizado en caso de fallo.
 
-# echo " "
-# echo "Obteniendo datos de la API..."
-# response=$(curl https://jsonplaceholder.typicode.com/users)
-
-# echo " "
-# echo "Datos obtenidos:"
-# echo "$response" | jq .
 
 echo " "
 echo "Obteniendo datos de la API..."
