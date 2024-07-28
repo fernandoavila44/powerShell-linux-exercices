91e0e712b3a5ba075e27c172713320249ae36aee
Verónica Ospina <baxico@msn.com>
2024-07-28 16:20:14 -0500

    Ejercicios Completados

--- Linux/Linux.sh
+++ Linux/Linux.sh
@@ -5,7 +5,7 @@
  #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
 
 echo "Lista de usuarios"
-response=$(curl https://jsonplaceholder.typicode.com/users)
+response=$(curl -s https://jsonplaceholder.typicode.com/users)
 echo "Datos obtenidos:"
 echo "$response" | jq .
 
@@ -14,7 +14,7 @@ echo "$response" | jq .
 #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
  
 echo "Títulos de las publicaciones:"
-response=$(curl https://jsonplaceholder.typicode.com/posts)
+response=$(curl -s https://jsonplaceholder.typicode.com/posts)
 echo "Datos obtenidos:"
 echo "$response" | jq ".[].title"
 
@@ -25,7 +25,7 @@ echo "$response" | jq ".[].title"
  
 POST_ID=2
 echo "Publicación con ID $POST_ID:"
-response=$(curl https://jsonplaceholder.typicode.com/posts/$POST_ID)
+response=$(curl -s https://jsonplaceholder.typicode.com/posts/$POST_ID)
 echo "Datos obtenidos:"
 echo "$response" | jq .
 
@@ -35,12 +35,12 @@ echo "$response" | jq .
 #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
  
 data='{
-    "title": "nuevo post",
-    "body": "Esto es un ejemplo post desde bash",
+    "title": "Nuevo post",
+    "body": "Ejemplo post desde bash",
     "userId": 2
 }'
 echo "Enviando un nuevo post:"
-response=$(curl -s -X POST -H "Content-Type: application/json; " -d "$data" https://jsonplaceholder.typicode.com/posts)
+response=$(curl -s -X POST -H "Content-Type: application/json" -d "$data" https://jsonplaceholder.typicode.com/posts)
 echo "Datos obtenidos:"
 echo "$response" | jq .
 
@@ -51,7 +51,7 @@ echo "$response" | jq .
 
 echo "Manejando un error:"
 RESPONSE=$(curl -s -o response.json -w "%{http_code}" https://jsonplaceholder.typicode.com/nonexistent_endpoint)
-if [ "$RESPONSE" -ne 200]; then
+if [ "$RESPONSE" -ne 200 ]; then
   echo "Error: Received HTTP status code $RESPONSE"
 else
   jq "." response.json
--- Powershell/Power.ps1
+++ Powershell/Power.ps1
@@ -1,26 +1,22 @@
 # 1. Crear una funcion que me diga si un número es par o no
-function parImpar {
-    param ($numero)
-    if (($numero % 2) -eq 0) {
-        Write-Output "$numero es par"
-    }else{
-        Write-Output "$numero es impar"
-    }    
+function Es_Par {
+    param(
+        [int]$number
+    )
+    if ($number % 2 -eq 0){
+        return "$number es un número par"
+    } else {
+        return "$number no es un número par"
+    }
 }
-
-parImpar 123
-
-
-
-
+$resultado = Es_Par
+Write-Output $resultado
 
 # 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
 # Devuelve Fizz si el número es divisible por 3.
 # Devuelve Buzz si el número es divisible por 5.
 # Devuelve FizzBuzz si el número es divisible por 3 y por 5.
-
-function fizzBuzz {
-    
+function FizzBuzz {
 for ($i = 1; $i -le 100; $i++) {
     if ($i % 3 -eq 0 -and $i % 5 -eq 0) {
         Write-Output "FizzBuzz"
@@ -33,50 +29,60 @@ for ($i = 1; $i -le 100; $i++) {
     }
 }
 }
-
-fizzBuzz
+FizzBuzz
 
 
 # 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
 
 $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-function existe(){
-    param($numero)
+function Exist_Number {
+    param (
+        [int]$numero
+    )
     
-    if([int]$numero -or $numero -eq 0){
-        if ($numeros -contains $numero){
-            Write-Output "El numero $numero existe en el arreglo."
-        }else{
-            Write-Output "El numero $numero no existe en el arreglo."
-        }
+    if ($numeros -contains $numero) {
+        return "$numero existe en el arreglo."
+    } else {
+        return "$numero no existe en el arreglo."
     }
 }
-existe 4
-existe 11
-
+Exist_Number 3
+Exist_Number 20
 
 # 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
-
-
 $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-function Suma {
-    $suma = $numeros | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-    return $suma
+
+function Sum_Array {
+    param (
+        [int[]]$array
+    )
+
+    $sum = 0
+    foreach ($num in $array){
+        $sum += $num
 }
 
-Write-Host "la suma de los numeros del arreglo es:"; Suma
+return $sum
+}
+$sum = Sum_Array -array $numeros
+Write-Output "La suma de los valores del arrego es: $sum"
 
 
 # 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
-
 $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-function sumaPares {
-    $total = 0
-    for ($i = 0; $i -lt $numeros.Length; $i++) {
-        if (($numeros[$i] % 2) -eq 0) {
-            $total = $total + $numeros[$i];
-        }
+
+function Sum_Pares {
+    param (
+        [int[]]$array
+    )
+    $sum = 0
+foreach ($num in $array){
+    if ($num % 2 -eq 0) {
+        $sum += $num
     }
-    Write-Output "La suma de los numeros pares del arreglo es $total" 
 }
-sumaPares
+return $sum
+    
+}
+$sum = Sum_Pares -array $numeros
+Write-Output "La suma de los números pares del arreglo es: $sum"
