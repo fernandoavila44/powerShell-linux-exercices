262e5b22d8fc3a6322cbfedcc7fb0aeba457723a
AlejoImB <alejoimb10@outlook.com>
2024-07-29 16:43:17 -0500

    Update Alejandro_Imbachi.sh

--- Linux/luis_lopez_taller.sh
+++ Linux/luis_lopez_taller.sh
@@ -8,7 +8,7 @@ echo "Lista de usuarios"
 response=$(curl https://jsonplaceholder.typicode.com/users)
 echo "Datos obtenidos:"
 echo "$response" | jq .
-
+f
 
 
 #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
