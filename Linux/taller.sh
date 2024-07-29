a89c924f275fe2f254429b9f899f49151906b320
Sandra Milena Sotelo Tobar <sandyosef1617@gmail.com>
2024-07-29 09:59:16 -0500

    tallerFinal

--- Linux/taller.sh
+++ Linux/taller.sh
@@ -1,9 +1,145 @@
+ #!/bin/bash
+ 
  # Utilizar la siguiente API https://jsonplaceholder.typicode.com/
 
+
+
+#SOLUCION DE EJERCICIOS
+
  #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
+echo " " 
+echo "RESPUESTA PUNTO NRO 1 ****************************************************************************"
+echo " "
+
+# Obtenemos los datos de la Api"
+response=$(curl https://jsonplaceholder.typicode.com/users)
+
+#Imprimo respuesta
+#echo $response
+
+#echo "Parseando el contenido"
+
+#Imprimo respuesta en formato JSON
+#echo "$response" | jq .
+
+echo " "
+
+#Capturamos los nombres de usuarios
+nombresUsuarios=$(echo "$response" | jq -r '.[].username')
+
+echo " "
+
+#Mostramos la lista de usuarios
+echo "Lista de Usuarios"
+echo " "
+#Con la variable entre comillas obtengo toda la lista de nombres
+echo "$nombresUsuarios"
+
+echo " "
+
  #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
+
+echo "RESPUESTA PUNTO NRO 2 ****************************************************************************"
+
+echo " "
+
+#echo "Obtenemos los datos de la Api"
+
+response=$(curl https://jsonplaceholder.typicode.com/posts)
+
+#Imprimo respuesta
+#echo $response
+
+#echo "Parseando el contenido"
+
+#Imprimo respuesta en formato JSON
+#echo "$response" | jq .
+echo " "
+#Capturamos los titulos de usuarios
+titulos=$(echo "$response" | jq -r '.[].title')
+
+echo " "
+
+#Con la variable entre comillas obtengo toda la lista de titulos
+echo "Los siguientes son los titulos de las publicaciones: "
+echo " "
+echo "$titulos "
+echo " "
+
  #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un id específico.
+
+echo "RESPUESTA PUNTO NRO 3 ****************************************************************************"
+echo " "
+
+#Le asigno a mi variable el id 3
+miId=3
+
+#Defino mi función consultar por ID
+consultarPorId() {
+    idRecibido=$1
+respuestaEspecifica=$(curl https://jsonplaceholder.typicode.com/posts/$1)
+
+#echo "Parseando el contenido con titulos"
+resConsulta=$(echo "$respuestaEspecifica" | jq -r '.title')
+
+#Imprimo los datos obtenidos de la consulta
+echo " "
+echo "La consulta por el ID $miId arrojo los siguientes datos:"
+echo " "
+echo "$resConsulta"
+
+}
+
+consultarPorId $miId
+
+echo " "
+ 
  #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
+ 
+
+echo "RESPUESTA PUNTO NRO 4 ****************************************************************************" 
+
+echo " "
+
+ # Creando el nuevo post y agragando datos personalizados
+nuevoP='{"title": "Canciones anos 80", "body": "Coleccion de canciones crossover", "userId": 1}'
+
+# Hacemos la solicitud
+respuesta=$(curl -X POST -H "Content-Type: application/json" -d "$nuevoP" "https://jsonplaceholder.typicode.com/posts")
+echo " "
+# Mostramos la respuesta
+echo "Respuesta del servidor:"
+echo "$respuesta"
+ 
+#Capturamos los datos de Id y Titulo con jq
+postId=$(echo "$respuesta" | jq -r '.id')
+postTitulo=$(echo "$respuesta" | jq -r '.title')
+
+# Mostramos los datos de ID y Titulo
+echo "Los datos de ID y Titulo del nuevo post son:"
+echo " "
+echo "El ID es: $postId"
+echo "El Título es: $postTitulo"
+echo " "
+
  #5. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/"cualquier endpoint" que devuelva un error intencionalmente. Utiliza jq para detectar y manejar el error, mostrando un mensaje personalizado en caso de fallo.
+
+echo "RESPUESTA PUNTO NRO 5 ****************************************************************************"
+echo " "
+
+# Hacemos la solicitud GET al endpoint "artistas", que en la realidad no existe
+
+respSolicitud=$(curl -s "https://jsonplaceholder.typicode.com/artistas")
+
+echo "Consultando por el endpoint "artistas""
+
+# Validamos si se ha encontrado algún campo "error" con el fin de mostrar el mensaje correspondiente
+echo " " 
+if [ "$respSolicitud" = "{}" ]; then
+    echo "Ha ocurrido un error en la consulta: la respuesta está vacía"
+else
+    echo "Respuesta exitosa:"
+    echo "$respSolicitud"
+fi
 
  #Exitos!!
--- Powershell/taller.ps1
+++ Powershell/taller.ps1
@@ -1,101 +1,129 @@
 # 1. Crear una funcion que me diga si un número es par o no
 
-# Definimos una función llamada Es-Par
-function Get-Par {
-    # Definimos un parámetro de entrada llamado $numero, que es de tipo entero
-    param (
-        [int]$numero
-    )
 
-    # Utilizamos el operador módulo (%) para comprobar si el número es divisible por 2
-    if ($numero % 2 -eq 0) {
-        # Si el residuo de la división por 2 es 0, el número es par
-        return "$numero es par."
-    } else {
-        # Si el residuo de la división por 2 no es 0, el número es impar
-        return "$numero es impar."
+# Se crea la funcion que me evaluará si un número es o no par.
+function numeroPar {
+    # declaramos la variable num que es de tipo entero la cual recibe por parámetro un número.
+    param ([int]$num)
+
+    # Evaluamos, si el modulo del número es cero entonces el numero es par de lo contrato es impar.
+    if ($num % 2 -eq 0) {
+        return "$num es un número par."
+    }
+    else {
+        return "$num es un número impar."
     }
 }
 
-# Ejemplo de uso: llamamos a la función Es-Par con el número 4
-Get-Par -numero 4
-# Ejemplo de uso: llamamos a la función Es-Par con el número 7
-Get-Par -numero 7
-
-
+# Hacemos el llamado de la función y le enviamos un número par y otro impar para evaluar el resultado
+numeroPar -num 10
+numeroPar -num 21
 
 # 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
 # Devuelve Fizz si el número es divisible por 3.
 # Devuelve Buzz si el número es divisible por 5.
 # Devuelve FizzBuzz si el número es divisible por 3 y por 5.
 
+# Creamos un ciclo for para recorrer los número del 1 al 100, cada número me lo evaluará con el módulo
+# dependiendo de si lo que necesito es saber si es divisible por 3, 5 o ambos
+
 for ($i = 1; $i -le 100; $i++) {
+
+    #Primero determinados si el número es divisible por 3 y 5
     if ($i % 3 -eq 0 -and $i % 5 -eq 0) {
-        Write-Output "FizzBuzz"
-    } elseif ($i % 3 -eq 0) {
-        Write-Output "Fizz"
-    } elseif ($i % 5 -eq 0) {
-        Write-Output "Buzz"
-    } else {
-        Write-Output $i
+        Write-Output "$i : FizzBuzz"
+
+    }
+
+    #Si el numero no cumple ambas condiciones evaluamos en que grupo esta, si en los divisibles por 3 o por 5
+    elseif ($i % 3 -eq 0) {
+        Write-Output "$i : Fizz"
+
+    }
+    elseif ($i % 5 -eq 0) {
+        Write-Output "$i : Buzz"
+
+    }
+    else {
+        Write-Output "$i : No aplica"
     }
 }
-
-
-
+   
 # 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
 # $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-# Definimos el arreglo de números
 $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-# Definimos una función llamada Find-Number
-function Find-Number {
+#Definimos la funcion Encontrar a la cual le enviamos por parámetro un número
+function Encontrar {
     param (
-        [int]$numero
+        [int]$num
     )
 
-    # Verificamos si el número existe en el arreglo
-    if ($numeros -contains $numero) {
-        return "El número $numero existe en el arreglo."
-    } else {
-        return "El número $numero no existe en el arreglo."
+    # Con el método contains verificamos si el número que recibimos está contenido en el arreglo.
+    if ($numeros -contains $num) {
+        return "El numero $num si existe en el arreglo numeros."
+    }
+    else {
+        return "El numero $num no existe en el arreglo."
     }
 }
-Find-Number -numero 5
-Find-Number -numero 11
 
-
+# Ahora llamamos la función para evaluar el resultado, para esto enviamos dos número por parámetro.
+    
+Encontrar -num 9
+Encontrar -num 21
 
 # 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
 
-# Definimos el arreglo de números
-$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# Defino el arreglo de números
+$numeritos = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-# Definimos una función llamada Get-Sum
-function Get-Sum {
-    # Calculamos la suma de todos los valores del arreglo
-    $suma = $numeros | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-    return $suma
+# Creo la funcion que me sumará todos los numeros del arreglo
+function sumaArreglo {
+ 
+    # En esta variable guardo la suma acumulada de los numeros del arreglo.
+    $sumaTotal = 0
+  
+    # Con este foreach recorremos el arreglo y la variable $num ira tomando 
+    #cada valor el cual sera sumado consecutivamente en cada iteración
+    foreach ($num in $numeritos) {
+        $sumaTotal += $num
+        
+    }
+
+    return "La suma total de los numeros del arreglo es: $sumaTotal"
 }
 
 # Ejemplo de uso: llamamos a la función Get-Sum
-Get-Sum
-
+sumaArreglo 
 
 
 # 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
 
-# Definimos el arreglo de números
-$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# Defino el arreglo de números
+$numeritos = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-# Definimos una función llamada Get-SumOfEvenNumbers
-function Get-SumOfEvenNumbers {
-    # Filtramos los números pares y calculamos la suma
-    $sumaPares = $numeros | Where-Object { $_ % 2 -eq 0 } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-    return $sumaPares
+# Creo la funcion que me sumará todos los numeros del arreglo
+function sumaArregloPares {
+ 
+    # En esta variable guardo la suma acumulada de los numeros del arreglo.
+    $sumaTotal = 0
+  
+    # Con este foreach recorremos el arreglo y la variable $num ira tomando 
+    #cada valor el cual sera sumado consecutivamente en cada iteración
+    foreach ($num in $numeritos) {
+        
+        # Con este if validamos que la suma solo se realice cuando el modulo 2 del numero del arreglo  sea cero. 
+        if ($num % 2 -eq 0) {
+            $sumaTotal += $num
+        }
+      
+        
+    }
+
+    return "La suma total de los numeros pares del arreglo es: $sumaTotal"
 }
 
-# Ejemplo de uso: llamamos a la función Get-SumOfEvenNumbers
-Get-SumOfEvenNumbers
-
+# Ejemplo de uso: llamamos a la función Get-Sum
+sumaArregloPares
