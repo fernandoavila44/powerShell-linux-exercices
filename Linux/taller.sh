5c3755197c7ff6f0ed8015d82813951f655e1778
Juan Reyes <juanmanuelreyesgarcia816@gmail.com>
2024-07-29 20:30:08 -0500

    Culminación talleres

--- Linux/taller.sh
+++ Linux/taller.sh
@@ -1,9 +1,44 @@
  # Utilizar la siguiente API https://jsonplaceholder.typicode.com/
 
  #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
+ 
+echo "Lista de usuarios"
+response=$(curl https://jsonplaceholder.typicode.com/users)
+echo "Datos obtenidos:"
+echo "$response" | jq .
  #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
- #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un id específico.
+
+echo "Títulos de las publicaciones:"
+response=$(curl https://jsonplaceholder.typicode.com/posts)
+echo "Datos obtenidos:"
+echo "$response" | jq ".[].title"
+
+ #3. Realiza una solicitud GET a la API jidsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un  específico.
+
+POST_ID=5
+echo "Publicación con ID $POST_ID:"
+response=$(curl https://jsonplaceholder.typicode.com/posts/$POST_ID)
+echo "Datos obtenidos:"
+echo "$response" | jq .
+
  #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
+
+data='{
+    "title": "nuevo post",
+    "body": "Esto es un ejemplo post desde bash",
+    "userId": 5
+}'
+echo "Enviando un nuevo post:"
+response=$(curl -s -X POST -H "Content-Type: application/json; " -d "$data" https://jsonplaceholder.typicode.com/posts)
+echo "Datos obtenidos:"
+echo "$response" | jq .
+
  #5. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/"cualquier endpoint" que devuelva un error intencionalmente. Utiliza jq para detectar y manejar el error, mostrando un mensaje personalizado en caso de fallo.
+
+echo "Manejando un error:"
+response=$(curl -s -o /dev/null -w '%{http_code}' https://jsonplaceholder.typicode.com/parqueSoft)
+if [ $response -eq 404 ]; then
+    echo "La página no fue encontrada. Error $response"
+fi
 
  #Exitos!!
--- Powershell/taller.ps1
+++ Powershell/taller.ps1
@@ -1,14 +1,118 @@
 # 1. Crear una funcion que me diga si un número es par o no
+function esPar{
+    param (
+        [int]$numero
+    )
+
+    if($numero -ne $null -and $numero -ne ""){
+        if(($numero % 2) -eq 0) {
+             Write-Output "El numero $numero es par"
+        } else {
+            Write-Output "El numero $numero es impar" 
+        }
+    } else {
+      
+        Write-Output "No se ha proporcionado ningun numero." 
+    }
+}
+esPar 17
 
 # 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
 # Devuelve Fizz si el número es divisible por 3.
 # Devuelve Buzz si el número es divisible por 5.
 # Devuelve FizzBuzz si el número es divisible por 3 y por 5.
 
+function fizzBuzz{
+    param (
+        [int]$numero
+    )
+
+    if($numero -ne $null -and $numero -ne ""){
+        if(($numero % 3 ) -eq 0 -and ($numero % 5 ) -eq 0 ) {
+             Write-Output "FizzBuzz"
+        } elseif (($numero % 3 ) -eq 0) {
+            Write-Output "Fizz" 
+        } elseif (($numero % 5 ) -eq 0) {
+            Write-Output "Buzz" 
+        } else {
+            Write-Output "El número no es divisible ni por 3 ni por 5 "
+        }
+    } else {
+      
+        Write-Output "No se ha proporcionado ningun numero." 
+    }
+}
+
+fizzBuzz 43
+
 # 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
 # $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+function numExiste{
+    param (
+        [int]$numero
+    )
+    $numrExist = $false
+    if($numero -ne $null -and $numero -ne ""){
+        foreach ($num in $numeros)
+        {
+            if($num -eq $numero){
+                $numrExist = $true
+            }
+        }
+        if($numrExist -eq $true){
+            Write-Output "El numero $numero existe en el arreglo numeros" 
+        } else {
+            Write-Output "El numero $numero no existe en el arreglo numeros" 
+        }
+
+    } else {
+      
+        Write-Output "No se ha proporcionado ningun numero." 
+    }
+}
+
+numExiste 28
+
 # 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
+
+$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+function sumNum{
+    param (
+        [int]$numero
+    )
+
+    $sum = 0
+
+    foreach ($num in $numeros)
+    {
+        $sum = $sum + $num
+        
+    }
+    Write-Output "La suma de todos los numeros es $sum" 
+}
+sumNum
+
 # 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
+$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+function sumNumPares{
+    param (
+        [int]$numero
+    )
+
+    $sum = 0
+
+    foreach ($num in $numeros)
+    {
+        if(($num % 2 ) -eq 0){
+            $sum = $sum + $num
+        } 
+        
+    }
+    Write-Output "La suma de todos los numeros pares es $sum" 
+}
+sumNumPares
 
 
