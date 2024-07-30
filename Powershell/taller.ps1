e14d3a02acba28b6dd31855214fdd5b6b871e523
Angel <riascomiguelangel53@gmail.com>
2024-07-29 21:12:52 -0500

    trabajo final

---
+++ Powershell/taller.ps1
@@ -0,0 +1,14 @@
+# 1. Crear una funcion que me diga si un número es par o no
+
+# 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
+# Devuelve Fizz si el número es divisible por 3.
+# Devuelve Buzz si el número es divisible por 5.
+# Devuelve FizzBuzz si el número es divisible por 3 y por 5.
+
+# 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
+# $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
+# 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
+
+
