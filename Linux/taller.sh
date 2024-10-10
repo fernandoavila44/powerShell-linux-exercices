53e8c652941878e66eabf0aa28a018e11f120849
Jose Malagon <josekye2@hotmail.com>
2024-10-09 23:35:04 -0500

    Jose Malagon

--- Linux/taller.sh
+++ Linux/taller.sh
@@ -1,9 +1,25 @@
- # Utilizar la siguiente API https://jsonplaceholder.typicode.com/
+# Utilizar la siguiente API https://jsonplaceholder.typicode.com/
 
  #1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
+echo "Obteniendo datos de la API..."
+ curl -s https://jsonplaceholder.typicode.com/users | jq
  #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
+echo "Obteniendo datos de la API..."
+ curl -s https://jsonplaceholder.typicode.com/posts | jq '.[].title'
  #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un id específico.
+echo "Obteniendo datos de la API..."    
+curl -s https://jsonplaceholder.typicode.com/posts | jq '.[] | select(.id == 5)'
  #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
+curl -s -X POST https://jsonplaceholder.typicode.com/posts \
+-H "Content-Type: application/json" \
+-d '{"title": "Mi nuevo post", "body": "Este es el contenido del post", "userId": 1}'| jq
  #5. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/"cualquier endpoint" que devuelva un error intencionalmente. Utiliza jq para detectar y manejar el error, mostrando un mensaje personalizado en caso de fallo.
+response=$(curl -s -w "%{http_code}" https://jsonplaceholder.typicode.com/invalidEndpoint -o response.json)
+
+if [ "$response" -ne 200 ]; then
+    echo "Error: La solicitud falló con el código de estado $response. El endpoint no existe."
+else
+    cat response.json | jq
+fi
 
  #Exitos!!
--- Powershell/taller.ps1
+++ Powershell/taller.ps1
@@ -1,14 +1,69 @@
 # 1. Crear una funcion que me diga si un número es par o no
+function Es-Par {
+    param (
+        [int]$numero
+    )
+    
+    if ($numero % 2 -eq 0) {
+        Write-Host "$numero es un número par."
+    } else {
+        Write-Host "$numero es un número impar."
+    }
+}
+
+Es-Par 8
+Es-Par 7
 
 # 2. Escribe un programa que imprima los números del 1 al 100, pero aplicando las siguientes normas:
 # Devuelve Fizz si el número es divisible por 3.
 # Devuelve Buzz si el número es divisible por 5.
 # Devuelve FizzBuzz si el número es divisible por 3 y por 5.
 
+for ($i = 1; $i -le 100; $i++) {
+    if ($i % 3 -eq 0 -and $i % 5 -eq 0) {
+        Write-Host "FizzBuzz"
+    } elseif ($i % 3 -eq 0) {
+        Write-Host "Fizz"
+    } elseif ($i % 5 -eq 0) {
+        Write-Host "Buzz"
+    } else {
+        Write-Host $i
+    }
+}
+
 # 3.Dado el siguiente arreglo, crear una funcion que me diga si el número que pasamos por parámetro existe en el arreglo
 # $numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+$numeros = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+function Existe-Numero {
+    param (
+        [int]$numero
+    )
+
+    if ($numeros -contains $numero) {
+        return "$numero existe en el arreglo."
+    } else {
+        return "$numero no existe en el arreglo."
+    }
+}
+
+Existe-Numero 5
+Existe-Numero 15
+
 # 4. Dado el arreglo anterior crear una funcion que me devuelva la suma de todos los valores del arreglo
+
+function Suma-Total {
+    return ($numeros | Measure-Object -Sum).Sum
+}
+
+Suma-Total
+
 # 5. Modificar la función anterior para que me devuelva solo la suma de los números pares
 
+function Suma-Pares {
+    $pares = $numeros | Where-Object { $_ % 2 -eq 0 }
+    return ($pares | Measure-Object -Sum).Sum
+}
+
+Suma-Pares
 
