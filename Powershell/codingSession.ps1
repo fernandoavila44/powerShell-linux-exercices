0e1a071c3fd7df929fdf6694a32adb25886b1785
Luis Casanova <luiscasanovaguerrero@gmail.com>
2024-11-16 09:28:26 -0500

    creacion rama luis_Casanova

--- Powershell/codingSession.ps1
+++ Powershell/codingSession.ps1
@@ -1,1 +1,10 @@
 #Hello
+#ejemplos
+#Para imprimir por pantalla
+"hola mundo"
+Write-Host "hola mundo Write-Host"
+echo "hola mundo echo"
+Write-Output "hola mundo Output"
+ 
+#para definir una variable se pone antes el $
+$miVariable = "soy una variable"
