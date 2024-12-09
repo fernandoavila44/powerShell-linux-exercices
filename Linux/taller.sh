6fa32b9dbae6da2641fc3738d471392cbcc9a813
Gabriel Aguilar <gabonfc1650@gmail.com>
2024-12-09 18:38:47 -0500

    Powershell

--- Linux/taller.sh
+++ Linux/taller.sh
@@ -1,5 +1,6 @@
 # Utilizar la siguiente API https://jsonplaceholder.typicode.com/
-#1. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
+#1. Realiza una solicitud GET a la API https://jsonplaceholder.typicode.com/users y utiliza jq para formatear y mostrar la lista de usuarios.
+
 #2. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para filtrar y mostrar solo los títulos de las publicaciones.
 #3. Realiza una solicitud GET a la API jsonplaceholder.typicode.com/posts y utiliza jq para encontrar y mostrar la publicación con un id específico.
 #4. Utiliza curl para enviar un nuevo post a la API jsonplaceholder.typicode.com/posts. Luego, utiliza jq para mostrar la respuesta del servidor.
